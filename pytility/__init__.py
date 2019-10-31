d2a29a3ca56ffc1fae96e92e09f41b090a4c9e23
Markus Shepherd <mk.schepke@gmail.com>
2019-10-31 16:08:09 +0200

    added iterables.window()

--- pytility/__init__.py
+++ pytility/__init__.py
@@ -4,6 +4,6 @@
 
 from .__version__ import VERSION, __version__
 from .files import concat_files
-from .iterables import arg_to_iter, batchify, clear_list, take_first
+from .iterables import arg_to_iter, batchify, clear_list, take_first, window
 from .parsers import parse_bool, parse_date, parse_float, parse_int
 from .strings import normalize_space, to_str
--- pytility/iterables.py
+++ pytility/iterables.py
@@ -3,14 +3,14 @@
 """Iterable utilities."""
 
 from collections import OrderedDict
-from itertools import groupby
-from typing import Any, Iterable, List, Optional, TypeVar
+from itertools import groupby, tee
+from typing import Any, Iterable, List, Optional, Tuple, TypeVar
 
 ITERABLE_SINGLE_VALUES = (dict, str, bytes)
-TYPE = TypeVar("TYPE")
+Typed = TypeVar("Typed")
 
 
-def clear_list(items: Iterable[Optional[TYPE]]) -> List[TYPE]:
+def clear_list(items: Iterable[Optional[Typed]]) -> List[Typed]:
     """Return unique items in order of first ocurrence."""
     return list(OrderedDict.fromkeys(filter(None, items)))
 
@@ -41,3 +41,15 @@ def batchify(iterable, size):
     """Make batches of given size."""
     for _, group in groupby(enumerate(iterable), key=lambda x: x[0] // size):
         yield (x[1] for x in group)
+
+
+def window(iterable: Iterable[Typed], size: int = 2) -> Iterable[Tuple[Typed, ...]]:
+    """Sliding window of an iterator."""
+
+    iterables = tee(iterable, size)
+
+    for num, itb in enumerate(iterables):
+        for _ in range(num):
+            next(itb, None)
+
+    return zip(*iterables)
